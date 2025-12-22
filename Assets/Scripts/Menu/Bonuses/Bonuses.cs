28db62d9210469ddda0dc4f25bdcc4785cd0bcff
sergeulub <122737478+sergeulub@users.noreply.github.com>
2025-12-22 23:53:57 +0300

    Reapply "Reapply "22.12.2025""
    This reverts commit 7a55323a87800ee32764f5f8ece72df2117e4f0d.

--- Assets/Scripts/Menu/Bonuses/Bonuses.cs
+++ Assets/Scripts/Menu/Bonuses/Bonuses.cs
@@ -16,9 +16,9 @@ namespace Bonuses {
             _item.upgradeMine.SetActive(false);
             _item.upgradeBonus.SetActive(true);
             _item.image.sprite = bonuses[index].sprite;
-            _item.rectTransformImage.sizeDelta = new Vector2(75, 75);
+            _item.rectTransformImage.sizeDelta  = new Vector2(75, 75);
             _item.name.text = bonuses[index].name;
-            _item.lvl.text = "Óð. " + (bonusesLvls[index] + 1).ToString();
+            _item.lvl.text = "ï¿½ï¿½. " + (bonusesLvls[index] + 1).ToString();
             _item.descriptionText.text = bonuses[index].description;
             _item.bonusOne.bonusNameText.text = bonuses[index].bonusName;
             _item.bonusOne.bonusValueText.text = (bonuses[index].bonusValue + bonuses[index].bonusUpgrade * bonusesLvls[curIndex]).ToString();
--- Assets/Scripts/Menu/MainMenuScripts/PlayerLoadout.cs
+++ Assets/Scripts/Menu/MainMenuScripts/PlayerLoadout.cs
@@ -7,16 +7,74 @@ public class PlayerLoadout : ScriptableObject
     // То, что реально идёт в бой (экипировка)
     public List<int> activeItems = new List<int>(6);
 
+    public List<int> itemsLevels = new List<int>(6);
+
     // Прокачка бонусов (itemID -> level)
-    public Dictionary<int, int> bonusLevels = new Dictionary<int, int>();
+    public List<int> bonusLevels = new List<int>(8);
 
     // Прокачка deployables (itemID -> level)
-    public Dictionary<int, int> deployableLevels = new Dictionary<int, int>();
+    public List<int> deployableLevels = new List<int>(4);
 
     public void Clear()
+    {   
+        for (int i = 0; i < 6; i++)
+        {
+            activeItems[i] = -1;
+            itemsLevels[i] = -1;
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            deployableLevels[i] = -1;
+        }
+        for (int i = 0; i < 8; i++)
+        {
+            bonusLevels[i] = -1;
+        }
+    }
+    public void FillPlayerLoadout(Info info)
     {
-        activeItems.Clear();
-        bonusLevels.Clear();
-        deployableLevels.Clear();
+        Clear();
+
+        InventoryData inventory = GameManager.InventoryData;
+        List<ItemInfo> allItems = info.GetAllItems();
+
+        // 🔹 1. Перенос экипировки (последние 6 ячеек)
+        // Обычно это 26–31, но используем константы
+            // --- 1. Забираем оружие из инвентаря ---
+            int weapon1ID = inventory.slotItemIDs[StaticDatas._weaponID1];
+            int weapon2ID = inventory.slotItemIDs[StaticDatas._weaponID2];
+            int bootsID = inventory.slotItemIDs[StaticDatas._bootsID];
+            int hatID = inventory.slotItemIDs[StaticDatas._hatID];
+            int ring1ID = inventory.slotItemIDs[StaticDatas._ringID1];
+            int ring2ID = inventory.slotItemIDs[StaticDatas._ringID2];
+
+
+            // --- 2. Нормализация порядка ---
+            if (weapon1ID < 0 && weapon2ID >= 0)
+            {
+                activeItems[0] = weapon2ID; // основное
+                itemsLevels[0] = inventory.levels[weapon2ID];
+
+                activeItems[1] = StaticDatas.EMPTY_SLOT;// второе пустое
+                itemsLevels[1] = -1;
+            }
+            else
+            {
+                activeItems[0] = weapon1ID;
+                itemsLevels[0] = inventory.levels[weapon1ID];
+                
+                activeItems[1] = weapon2ID;
+                itemsLevels[1] = inventory.levels[weapon2ID];
+            }
+
+            // --- 3. Остальная экипировка (без логики сдвига) ---
+            activeItems[2] = bootsID;
+            activeItems[3] = hatID;
+            activeItems[4] = ring1ID;
+            activeItems[5] = ring2ID;
+            
+        // 🔹 2. Перенос прокачки бонусов и deployables
+        bonusLevels = GameManager.ArtefactsData.boostsLevels;
+        deployableLevels = GameManager.ArtefactsData.deployablesLevels;
     }
 }
